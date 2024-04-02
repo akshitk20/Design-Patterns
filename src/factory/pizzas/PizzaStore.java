57521811215900db7478e88f184b35dd9039a140
Akshit Khatri <akshitkhatri.20@gmail.com>
2024-04-02 20:11:02 +0530

    added factory pattern

---
+++ src/factory/pizzas/PizzaStore.java
@@ -0,0 +1,19 @@
+package factory.pizzas;
+
+public class PizzaStore {
+
+    PizzaFactory pizzaFactory;
+
+    public PizzaStore(PizzaFactory pizzaFactory) {
+        this.pizzaFactory = pizzaFactory;
+    }
+
+    public Pizza orderPizza(String type) {
+        Pizza pizza = pizzaFactory.createPizza(type);
+        pizza.prepare();
+        pizza.bake();
+        pizza.cut();
+        pizza.box();
+        return pizza;
+    }
+}
---
+++ src/factory/pizzas/PizzaTestDrive.java
@@ -0,0 +1,16 @@
+package factory.pizzas;
+
+public class PizzaTestDrive {
+    public static void main(String[] args) {
+        PizzaFactory factory = new PizzaFactory();
+        PizzaStore store = new PizzaStore(factory);
+
+        Pizza pizza = store.orderPizza("cheese");
+        System.out.println("We ordered a " + pizza.getName() + "\n");
+        System.out.println(pizza);
+
+        pizza = store.orderPizza("veggie");
+        System.out.println("We ordered a " + pizza.getName() + "\n");
+        System.out.println(pizza);
+    }
+}
