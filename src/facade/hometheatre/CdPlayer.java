951fe2d04596f8c19fa6632705d9caf4c0cab1ba
Akshit Khatri <akshitkhatri.20@gmail.com>
2024-04-04 19:47:40 +0530

    facade pattern added

---
+++ src/facade/hometheatre/CdPlayer.java
@@ -0,0 +1,42 @@
+package facade.hometheatre;
+
+public class CdPlayer {
+    String description;
+    int currentTrack;
+    Amplifier amplifier;
+    String title;
+
+    public CdPlayer(String description, Amplifier amplifier) {
+        this.description = description;
+        this.amplifier = amplifier;
+    }
+
+    public void on() {
+        System.out.println(description + " on");
+    }
+
+    public void off() {
+        System.out.println(description + " off");
+    }
+
+    public void eject() {
+        title = null;
+        System.out.println(description + " eject");
+    }
+
+    public void play(String title) {
+        this.title = title;
+        currentTrack = 0;
+        System.out.println(description + " playing \"" + title + "\"");
+    }
+
+    public void play(int track) {
+        if (title == null) {
+            System.out.println(description + " can't play track " + currentTrack +
+                    ", no cd inserted");
+        } else {
+            currentTrack = track;
+            System.out.println(description + " playing track " + currentTrack);
+        }
+    }
+}
